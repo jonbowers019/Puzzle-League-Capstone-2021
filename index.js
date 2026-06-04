bc3eee969f971e827421e311648de61249c4200a
jonisrael <jonisrael45@gmail.com>
2026-06-04 10:21:25 -0500

    fixed button crash

--- index.js
+++ index.js
@@ -59,8 +59,6 @@ export function render(st) {
     // showPatchNotes();
     localStorage.setItem("patchNotesShown", "true");
   }
-  document.getElementById("browser").innerHTML = win.browser;
-  document.getElementById("os").innerHTML = win.os;
   addEventListeners(st);
 }
 
